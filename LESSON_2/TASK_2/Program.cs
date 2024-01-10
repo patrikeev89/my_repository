11baf7204c1df69f979002af2f075ced6edea6b3
PATRIKEEV_89 <patrikeev89@gmail.com>
2024-01-10 19:39:07 +0500

    add Lesson2:Task2

---
+++ LESSON_2/TASK_2/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+while (i < n)
+{
+    arr[i] = i + 1;
+    // Console.Write(arr[i]);
+    // Console.Write(' ');
+    Console.Write($"{arr[i]} "); // то же самое, что и 2 верхние строчки
+    i = i + 1;
+}
